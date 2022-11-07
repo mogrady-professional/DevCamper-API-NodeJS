34eea099952c3fa80adaeadf1fb1f63680f6db65
Michael O'Grady <mogrady.professional@gmail.com>
2022-11-07 12:40:35 +0000

    login functionality added

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express'); // Import express
-const { register } = require('../controllers/auth'); // Import register controller
+const { register, login } = require('../controllers/auth'); // Import register controller
 
 const router = express.Router(); // Create a router
 
 router.post('/register', register); // POST /api/v1/auth/register
+router.post('/login', login); // POST /api/v1/auth/login
 
 module.exports = router; // Export the router
