53244b638be23667d4ddc8071dff3540a635456d
Michael O'Grady <mogrady.professional@gmail.com>
2022-11-10 16:17:56 +0000

    cookie parser feature added; to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,9 +17,10 @@ exports.register = asyncHandler(async (req, res, next) => {
   });
 
   // Create token
-  const token = user.getSignedJwtToken();
+  //   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
+  //   res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc   Login user
@@ -48,8 +49,30 @@ exports.login = asyncHandler(async (req, res, next) => {
   }
 
   // Create token
-  const token = user.getSignedJwtToken();
+  //   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
-  //   sendTokenResponse(user, 200, res);
+  //   res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token
+  const token = user.getSignedJwtToken();
+  // JWT cookie options
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  });
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express'); // Import express
 const dotenv = require('dotenv'); // Import dotenv
 const colors = require('colors'); // Import colors
 const fileupload = require('express-fileupload'); // Import express-fileupload
+const cookieParser = require('cookie-parser'); // Import cookie-parser
 // Custom Logger
 // const logger = require('./middleware/logger'); // Import logger middleware
 // Morgan HTTP request logger middleware for node.js
